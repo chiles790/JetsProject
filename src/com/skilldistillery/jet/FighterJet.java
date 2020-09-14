5f5bb6ad80cb0e80b2df17ba07da3b5663cff807
Chris Hiles <chrishiles790@gmail.com>
2020-09-14 07:39:24 -0600

    fighterjet

---
+++ src/com/skilldistillery/jet/FighterJet.java
@@ -0,0 +1,16 @@
+package com.skilldistillery.jet;
+
+public class FighterJet extends Jet{
+	
+	public FighterJet(String model, double mph, int range, long price, double mach) {
+		super(model, mph, range, price, mach);
+	}
+
+
+
+	public void fight() {
+		
+	}
+
+
+}
